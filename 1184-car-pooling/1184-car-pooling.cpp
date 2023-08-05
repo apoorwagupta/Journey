b67d743b25a42cba06698e99b31cf4390de957c2
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2023-08-05 14:02:24 +0530

    Time: 9 ms (92.7%), Space: 10.8 MB (25.4%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.cpp
+++ 1184-car-pooling/1184-car-pooling.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         vector<int> fill(1002,0);
@@ -8,5 +8,21 @@ public:
                  if(fill[i]>capacity) return false ;
          } }
         return true;
+
     }
-};
+}; */
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> fill(1001,0);
+        for(auto trip : trips) {
+            fill[trip[1]] +=trip[0];
+            fill[trip[2]] -=trip[0];
+        }
+        int seating =0;
+        for(int i= 0;i<fill.size();i++) {
+           seating+= fill[i];
+           if(seating>capacity) return false ;
+        }
+        return true;}};
