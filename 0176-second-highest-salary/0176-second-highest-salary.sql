bdf847a733076883460f6ea9fb97267294ce8dc8
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2024-08-01 00:09:57 +0530

    Time: 311 ms (19.62%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,13 @@
-WITH cte AS (
-    SELECT salary, 
-           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-    FROM employee
-)
-SELECT 
-    (SELECT salary 
-     FROM cte 
-     WHERE rnk = 2
-     LIMIT 1) AS SecondHighestSalary;
+-- WITH cte AS (
+--     SELECT salary, 
+--            DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+--     FROM employee
+-- )
+-- SELECT 
+--     (SELECT salary 
+--      FROM cte 
+--      WHERE rnk = 2
+--      LIMIT 1) AS SecondHighestSalary;
+
+select max(salary) AS SecondHighestSalary FROM employee 
+where salary < (select max(salary) FROM employee );
