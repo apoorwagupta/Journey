13f18b4ef656cbc8e4b7524d5ff02d81ca9ca499
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2024-08-11 23:04:13 +0530

    Time: 122 ms (52.06%), Space: 81 MB (47.09%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,8 +3,10 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
-        if(mp.find(nums[i])!=mp.end() && abs(i-mp[nums[i]])<=k) return true;
-        mp[nums[i]]=i;
+       
+        if(mp.find(nums[i])!=mp.end() && abs(i-mp[nums[i]])<=k) return true; // present and diff is less
+        mp[nums[i]]=i; // not present, update; or if present and not satisfying k,add recent value 
+
         }
         return false;
     }
