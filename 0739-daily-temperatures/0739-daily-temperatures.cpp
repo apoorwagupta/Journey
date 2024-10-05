d98d5dcbcdc0196cc3a013878d915367eef9a61c
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2024-10-05 14:58:00 +0530

    Time: 118 ms (63.17%), Space: 101.3 MB (87.53%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+    int n = temperatures.size();
+    vector<int>nge(n, 0); // initially all 0, stores distance between their next greater element 
+    // and current temperature
+    stack<int>st{}; 
+    for(int i=n-1;i>=0;--i){
+        while(!st.empty() && temperatures[st.top()]<=temperatures[i]) st.pop();
+        if(!st.empty()) nge[i] = st.top()-i; 
+        st.push(i);
+    }
+    return nge;
+    }
+};
