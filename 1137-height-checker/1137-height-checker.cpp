211f828fd51289c6c12d37cc686a75038156f08e
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2024-10-06 18:44:16 +0530

    Time: 2 ms (48.95%), Space: 10.3 MB (86.28%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -23,10 +23,10 @@ public:
     int hptr =1;
     int cnt=0;
     vector<int> h(101);
-    for(int i = 0;i<n;i++) h[heights[i]]++;
+    for(int i = 0;i<n;i++) h[heights[i]]++; 
     for(int i = 0;i<n;i++){
-        while(h[hptr]==0) hptr++;
-        if(heights[i]!=hptr) cnt++;
+        while(h[hptr]==0) hptr++; //smallest height that should be next in sorted order
+        if(heights[i]!=hptr) cnt++; // If the current student's height is not in the correct position
         h[hptr]--;
       }
       return cnt;
