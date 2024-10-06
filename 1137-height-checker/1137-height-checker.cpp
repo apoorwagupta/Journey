88d872eb9f79ae8d7afb3f448d9a1c0f9c6d1bd0
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2024-10-06 18:42:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (86.28%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+ 
+//     int heightChecker(vector<int>& heights) {
+//     int n = heights.size();
+//     int cnt=0;
+//     vector<int> h = heights;
+//     sort(heights.begin(),heights.end());
+    
+//     for(int i = 0;i<n;i++){
+//         //  if(heights[i-1]>heights[i] || heights[i+1]<heights[i]) break;
+//         if(heights[i]!=h[i]) cnt++;
+//       }
+//       return cnt;
+//     }
+// };
+
 class Solution {
 public:
+ 
     int heightChecker(vector<int>& heights) {
     int n = heights.size();
+    int hptr =1;
     int cnt=0;
-    vector<int> h = heights;
-    sort(heights.begin(),heights.end());
-    
+    vector<int> h(101);
+    for(int i = 0;i<n;i++) h[heights[i]]++;
     for(int i = 0;i<n;i++){
-        //  if(heights[i-1]>heights[i] || heights[i+1]<heights[i]) break;
-        if(heights[i]!=h[i]) cnt++;
+        while(h[hptr]==0) hptr++;
+        if(heights[i]!=hptr) cnt++;
+        h[hptr]--;
       }
       return cnt;
     }
