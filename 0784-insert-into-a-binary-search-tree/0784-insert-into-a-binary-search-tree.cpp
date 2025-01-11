93b5975b5946077e12a8bcb2ceecfdba80953b72
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2025-01-12 01:45:24 +0530

    Time: 0 ms (100%), Space: 59.2 MB (61.6%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -9,12 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// recursion 
+/*
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
     if(root==NULL) return new TreeNode(val);
     if(val>root->val) root->right = (root->right!=NULL)? insertIntoBST(root->right,val):new TreeNode(val);
     else if (val<root->val) root->left = (root->left!=NULL)? insertIntoBST(root->left,val):new TreeNode(val);
+    return root;
+    }
+};
+
+*/
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+    if(root==NULL) return new TreeNode(val);
+    TreeNode* temp = new TreeNode(val);
+    TreeNode* parent= NULL,*curr=root;
+    while(curr!=NULL){
+        parent = curr;
+        if(curr->val<val) curr = curr->right;
+        else if(curr->val>val) curr = curr->left;
+        else return root;
+    }
+
+    if(parent->val> val) parent->left = temp;
+    else parent->right = temp;
     return root;
     }
 };
