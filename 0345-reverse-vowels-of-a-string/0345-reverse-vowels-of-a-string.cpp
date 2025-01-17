2f3a247e80ce173a76e25565c8491cf285cd6c38
Apoorwa Gupta <apoorwa.gupta01@gmail.com>
2025-01-17 21:39:42 +0530

    Time: 0 ms (100%), Space: 10.4 MB (45.84%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,18 +1,36 @@
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         string ans = s;
+//         int start =0;
+//         int end = s.length()-1;
+//         string vowels = "aeiouAEIOU";
+//         while(start<end){
+//             while(start<end && vowels.find(ans[start])==-1) start++;
+//             while(start<end  && vowels.find(ans[end])==-1) end--;
+
+//             swap(ans[start], ans[end]);
+//             start++;
+//             end--;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string reverseVowels(string s) {
-        string ans = s;
-        int start =0;
-        int end = s.length()-1;
-        string vowels = "aeiouAEIOU";
-        while(start<end){
-            while(start<end && vowels.find(ans[start])==-1) start++;
-            while(start<end  && vowels.find(ans[end])==-1) end--;
-
-            swap(ans[start], ans[end]);
-            start++;
-            end--;
-        }
-        return ans;
+    int start = 0;
+    int end = s.length()-1;
+    string ans = s;
+    string vowels = "aeiouAEIOU";
+    while(start<end){
+        while(start<end  && vowels.find(s[start])==-1) start++;
+        while(start<end  && vowels.find(s[end])==-1) end--;
+        swap(ans[start], ans[end]);
+        start++;
+        end--;
     }
-};
+    return ans;
+    }
+    };
